1e9965cae9aa9a30fc79b7f56db3028df93d0f4c
cfisher <cfisher@redlinetek.com>
2016-07-10 18:21:20 -0700

    init

---
+++ SU_Shooter_demo/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public float speed = 6f;
+
+	Vector3 movement;
+	//Animator anim;
+	Rigidbody playerRigidbody;
+	int floorMask;
+	float camRayLength = 100f;
+
+	void Awake() //use to create refrences whether script is enabled or not
+	{
+		floorMask = LayerMask.GetMask("Floor");
+		//anim = GetComponent<Animator>();
+		playerRigidbody = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate()
+	{
+		float h = Input.GetAxisRaw("Horizontal"); //raw gets 1, 0, -1 without ramp up
+		float v = Input.GetAxisRaw("Vertical");
+
+		Move(h, v);
+		Turning();
+		//Animating(h, v);
+	}
+
+	void Move(float h, float v)
+	{
+		movement.Set(h, 0f, v);
+		movement = movement.normalized * speed * Time.deltaTime; //normalize to set to 1,0 //deltatime is per second
+		playerRigidbody.MovePosition(transform.position + movement);
+	}
+
+	void Turning()
+	{
+		Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit floorHit;
+
+		if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
+		{
+			Vector3 playerToMouse = floorHit.point - transform.position;
+			playerToMouse.y = 0f;
+
+			Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+			playerRigidbody.MoveRotation(newRotation);
+		}
+	}
+
+	void Animating(float h, float v)
+	{
+		bool walking = h != 0f || v != 0f;
+		//anim.SetBool("IsWalking", walking);
+	}
+}
